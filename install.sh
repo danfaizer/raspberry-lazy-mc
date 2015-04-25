1eab8e95c6e18a8bb4952526c8b2807940fbf2d8
Daniel <danfaizer@gmail.com>
2015-04-25 10:36:20 +0200

    Testing

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ cd /tmp
 rm -rf /tmp/raspberry-lazy-mc
 git clone https://github.com/danfaizer/raspberry-lazy-mc
 cd /tmp/raspberry-lazy-mc
-ansible-playbook -i "localhost," -c local raspberry-lazy-mc.yml
+ansible-playbook -i "localhost," -c local raspberry-lazy-mc.yml "$@"
