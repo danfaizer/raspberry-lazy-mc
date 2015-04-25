9e92bc6986f930b70588ea163a331a3db56811e9
Daniel <danfaizer@gmail.com>
2015-04-25 09:49:23 +0200

    Testing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # raspberry-lazy-mc
 Raspberry + Transmission + CouchBase + SickRage + Kodi
+
+
+curl -L https://raw.githubusercontent.com/danfaizer/raspberry-lazy-mc/master/install.sh  | bash
--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install git -y
-pip install ansible
+sudo apt-get update
+sudo apt-get install git python-pip python-dev -y
+sudo pip install ansible
 cd /tmp
+rm -rf /tmp/raspberry-lazy-mc
 git clone https://github.com/danfaizer/raspberry-lazy-mc
 cd /tmp/raspberry-lazy-mc
-ansible-playbook raspberry-lazy-mc.yml
+ansible-playbook -i "localhost," -c local raspberry-lazy-mc.yml
